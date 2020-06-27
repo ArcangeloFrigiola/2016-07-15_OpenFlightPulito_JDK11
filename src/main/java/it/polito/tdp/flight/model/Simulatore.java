87ef56d720e933fb9b0abb0173e8b90628bd5f10
arcan <arcan@192.168.1.8>
2020-06-27 17:36:25 +0200

    Tema d'esame

--- src/main/java/it/polito/tdp/flight/model/Simulatore.java
+++ src/main/java/it/polito/tdp/flight/model/Simulatore.java
@@ -1,8 +1,6 @@
 package it.polito.tdp.flight.model;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,11 +37,16 @@ public class Simulatore {
 		this.queue=new PriorityQueue<>();
 		this.aeroportoPersone=new HashMap<>();
 		
+		for(Airport a: this.listaAeroporti) {
+			this.aeroportoPersone.put(a, 0);
+		}
+		
 		for(int i=0; i<this.passeggeri; i++) {
 			
 			int random = Math.round((float)(Math.random()*this.grafo.vertexSet().size()));
 			Event e = new Event(EventType.PARTENZA, i, this.listaAeroporti.get(random), this.dataPartenza);
 			this.queue.add(e);
+			this.aeroportoPersone.replace(this.listaAeroporti.get(random), 1);
 		}
 	}
 
@@ -54,30 +57,41 @@ public class Simulatore {
 			Event e = this.queue.poll();
 			processEvent(e);
 		}
+		
 	}
 
 	private void processEvent(Event e) {
 		
+		List<Airport> temp = Graphs.successorListOf(this.grafo, e.getAeroportoPartenza());
 		switch(e.getTipoEvento()) {
 		
 		case PARTENZA:
 			
-			List<Airport> temp = Graphs.successorListOf(this.grafo, e.getAeroportoPartenza());
+			
 			
 			if(temp.size()==0) {
 				break;
 			} else {
-				int random = Math.round((float) (Math.random() * temp.size()));
+				int random = Math.round((float) (Math.random() *( temp.size()-1)));
 				Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), temp.get(random),
 						this.dataPartenza.plusHours(1 + Math.round((float) this.grafo
 								.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp.get(random))))));
 				this.queue.add(nuovo);
+				
+				this.aeroportoPersone.replace(e.getAeroportoPartenza(), 
+						(this.aeroportoPersone.get(e.getAeroportoPartenza())-1));
+				this.aeroportoPersone.replace(this.listaAeroporti.get(random), (this.aeroportoPersone.get(this.listaAeroporti.get(random))+1));
+				break;
 			}
-			break;
+			
 			
 		case VIAGGIO:
 			
-			List<Airport> temp2 = Graphs.successorListOf(this.grafo, e.getAeroportoPartenza());
+			if(temp.size()==0) {
+				Event nuovo = new Event(EventType.STOP, e.getIdPasseggero(), e.getAeroportoPartenza(), e.getOra());
+				this.queue.add(nuovo);
+				break;
+			}
 			if(e.getOra().compareTo(dataPartenza.plusHours(48))>=0) {
 				Event nuovo = new Event(EventType.STOP, e.getIdPasseggero(), e.getAeroportoPartenza(), e.getOra());
 				this.queue.add(nuovo);
@@ -91,28 +105,58 @@ public class Simulatore {
 						this.queue.add(nuovo);
 						break;
 					}else {
-						int random = Math.round((float) (Math.random() * temp2.size()));
+						int random = Math.round((float) (Math.random() * (temp.size()-1)));
 						Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), e.getAeroportoPartenza(),
 								e.getOra().plusHours((7-e.getOra().getHour())+Math.round((float) this.grafo
-										.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp2.get(random))))));
+										.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp.get(random))))));
 						this.queue.add(nuovo);
+						this.aeroportoPersone.replace(e.getAeroportoPartenza(), 
+								this.aeroportoPersone.get(e.getAeroportoPartenza())-1);
+						this.aeroportoPersone.replace(this.listaAeroporti.get(random), this.aeroportoPersone.get(this.listaAeroporti.get(random))+1);
 						break;
 					}
 					
 				}else {
 					
-					int random = Math.round((float) (Math.random() * temp2.size()));
-					Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), temp2.get(random),
-							this.dataPartenza.plusHours(1 + Math.round((float) this.grafo
-									.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp2.get(random))))));
-					this.queue.add(nuovo);
-				
-					break;
+					if(e.getOra().getHour()%2!=0) {
+						
+						int random = Math.round((float) (Math.random() * (temp.size()-1)));
+						Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), temp.get(random),
+								e.getOra().plusHours(Math.round((float) this.grafo
+										.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp.get(random))))));
+						this.queue.add(nuovo);
+						this.aeroportoPersone.replace(e.getAeroportoPartenza(), 
+								this.aeroportoPersone.get(e.getAeroportoPartenza())-1);
+						this.aeroportoPersone.replace(this.listaAeroporti.get(random), this.aeroportoPersone.get(this.listaAeroporti.get(random))+1);
+					
+						break;
+					}else {
+						int random = Math.round((float) (Math.random() * (temp.size()-1)));
+						Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), temp.get(random),
+								e.getOra().plusHours(1 + Math.round((float) this.grafo
+										.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp.get(random))))));
+						this.queue.add(nuovo);
+						this.aeroportoPersone.replace(e.getAeroportoPartenza(), 
+								this.aeroportoPersone.get(e.getAeroportoPartenza())-1);
+						this.aeroportoPersone.replace(this.listaAeroporti.get(random), this.aeroportoPersone.get(this.listaAeroporti.get(random))+1);
+					
+						break;
+					}
+					
 				}
 				
 			}
 			
 		case PAUSE:
+			
+			int ran = Math.round((float) (Math.random() * (temp.size()-1)));
+			Event nuovo = new Event(EventType.VIAGGIO, e.getIdPasseggero(), e.getAeroportoPartenza(),
+					e.getOra().plusHours((7-e.getOra().getHour())+Math.round((float) this.grafo
+							.getEdgeWeight(this.grafo.getEdge(e.getAeroportoPartenza(), temp.get(ran))))));
+			this.queue.add(nuovo);
+			this.aeroportoPersone.replace(e.getAeroportoPartenza(), 
+					this.aeroportoPersone.get(e.getAeroportoPartenza())-1);
+			this.aeroportoPersone.replace(this.listaAeroporti.get(ran), this.aeroportoPersone.get(this.listaAeroporti.get(ran))+1);
 			break;
 			
 		case STOP:
@@ -124,7 +168,7 @@ public class Simulatore {
 
 	public Map<Airport, Integer> getMappaAeroportiPasseggeri() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.aeroportoPersone;
 	}
 
 }
